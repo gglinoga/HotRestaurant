05337cf0e8a07d7dee4ea9398385dd2808f0f30a
melissa maguire <melissapaigemaguire98@gmail.com>
2019-04-16 13:50:39 -0700

    constructor-function

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-
+// Create a set of variables (hint: arrays of objects) for holding the reservation and waitlist data
+function Table(name,phoneNumber,email,uniqueId) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.uniqueId = uniqueId;
+}
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
     extended: true
